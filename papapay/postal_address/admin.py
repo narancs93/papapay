6cf92238db326762c88741b57b57a42691acfd3b
Lajos Seyler <lajos.seyler@gmail.com>
2023-12-17 22:07:17 +0100

    feat - Add models to admin page

---
+++ papapay/postal_address/admin.py
@@ -0,0 +1,43 @@
+from django.contrib import admin
+
+from .models import City, Country, District, PostalAddress, State, Street
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'initials')
+    search_fields = ('name', 'initials')
+
+
+@admin.register(State)
+class StateAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'initials', 'area_code', 'country')
+    list_filter = ('country',)
+    search_fields = ('name', 'initials', 'area_code', 'country')
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'state')
+    list_filter = ('state__country', 'state')
+    search_fields = ('name', 'state')
+
+
+@admin.register(District)
+class DistrictAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'city')
+    list_filter = ('city',)
+    search_fields = ('name', 'city')
+
+
+@admin.register(Street)
+class StreetAdmin(admin.ModelAdmin):
+    list_display = ('id', 'zip_code', 'name', 'district')
+    list_filter = ('district__city', 'district')
+    search_fields = ('zip_code', 'name', 'district')
+
+
+@admin.register(PostalAddress)
+class PostalAddressAdmin(admin.ModelAdmin):
+    list_display = ('id', 'street', 'house_number', 'floor_number', 'door_number', 'note')
+    search_fields = ('street', 'house_number', 'floor_number', 'door_number', 'note')
