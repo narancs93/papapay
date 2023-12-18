24c4224ae37a595573f2f10977686f5923682f26
Lajos Seyler <lajos.seyler@gmail.com>
2023-12-18 18:05:12 +0100

    refactor - Rename fields

--- papapay/common/models.py
+++ papapay/common/models.py
@@ -8,19 +8,15 @@ class PhoneNumber(models.Model):
     name = models.CharField(max_length=64)
     country = models.ForeignKey(Country, related_name='phone_numbers', on_delete=models.PROTECT)
     phone_number = models.CharField(max_length=20)
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    owner = GenericForeignKey("content_type", "object_id")
-
-    def save(self, *args, **kwargs):
-        self.full_clean()
-        super().save(*args, **kwargs)
+    owner_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    owner_id = models.PositiveIntegerField()
+    owner = GenericForeignKey("owner_type", "owner_id")
 
     def __str__(self):
         return f'{self.name} (id={self.id})'
 
     class Meta:
         indexes = [
-            models.Index(fields=["content_type", "object_id"]),
+            models.Index(fields=["owner_id", "owner_id"]),
         ]
         unique_together = ('country', 'phone_number')
