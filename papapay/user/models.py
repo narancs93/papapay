39d22fff1574e214fba0ef4fe9dc40c3f518c20c
Lajos Seyler <lajos.seyler@gmail.com>
2023-12-20 17:04:57 +0100

    feat - Change USERNAME_FIELD to 'email'

--- papapay/user/models.py
+++ papapay/user/models.py
@@ -1,5 +1,16 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
