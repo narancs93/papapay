3a465e6124ec9c2521b4f9c9d72cc393176d8895
Lajos Seyler <lajos.seyler@gmail.com>
2023-12-20 17:17:27 +0100

    feat - Make first_name and last_name mandatory

--- papapay/user/models.py
+++ papapay/user/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 
 class User(AbstractUser):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
     email = models.EmailField(
         verbose_name='email address',
         max_length=255,
