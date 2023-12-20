e7833239c1e3cc82a7bb3362741e2320c37ac99c
Lajos Seyler <lajos.seyler@gmail.com>
2023-12-20 19:36:25 +0100

    feat - Implement login url

--- papapay/user/serializers.py
+++ papapay/user/serializers.py
@@ -1,7 +1,7 @@
-from rest_framework import serializers
-from django.contrib.auth import get_user_model
-from rest_framework.validators import UniqueValidator
+from django.contrib.auth import authenticate, get_user_model
 from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 User = get_user_model()
 
@@ -43,3 +43,41 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.save()
 
         return user
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.CharField(max_length=255, required=False)
+    password = serializers.CharField(
+        required=False,
+        label=("Password"),
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+        max_length=128,
+        write_only=True
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user = None
+
+    def get_authenticated_user(self):
+        return self.user
+
+    def validate(self, data):
+        self.validate_email_and_password_not_empty(data)
+        self.user = authenticate(
+            request=self.context.get('request'), email=data.get('email'), password=data.get('password'))
+        if self.user is None:
+            raise serializers.ValidationError({'password': 'Invalid username or password'})
+        return data
+
+    def validate_email_and_password_not_empty(self, data):
+        validation_errors = {}
+
+        if not data.get('email'):
+            validation_errors['email'] = 'Email address is required. Please enter a valid email.'
+        if not data.get('password'):
+            validation_errors['password'] = 'Password is required.'
+
+        if validation_errors:
+            raise serializers.ValidationError(validation_errors)
--- papapay/user/views.py
+++ papapay/user/views.py
@@ -1,10 +1,10 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, login
 from django.shortcuts import redirect
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import RegisterSerializer
+from .serializers import LoginSerializer, RegisterSerializer
 
 User = get_user_model()
 
@@ -23,4 +23,25 @@ class SignupView(APIView):
         if not serializer.is_valid():
             return Response({'serializer': serializer, 'style': self.style})
         serializer.save()
-        return redirect('papapay.home:home-url')
+        return redirect('papapay.user:login')
+
+
+class LoginView(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'user/login.html'
+    style = {'template_pack': 'user/serializers/horizontal'}
+
+    def get(self, request):
+        serializer = LoginSerializer()
+        return Response({'serializer': serializer, 'style': self.style})
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.POST, context={'request': request})
+
+        if serializer.is_valid():
+            user = serializer.get_authenticated_user()
+            login(request, user)
+            return redirect('papapay.home:home-url')
+
+        else:
+            return Response({'serializer': serializer, 'style': self.style})
