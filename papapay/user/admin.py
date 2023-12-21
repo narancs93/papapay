52b4ee449ba8bbd063f53de9c6244075a6c3bf3c
Lajos Seyler <lajos.seyler@gmail.com>
2023-12-21 11:21:38 +0100

    feat - Remove username field from user

--- papapay/user/admin.py
+++ papapay/user/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("first_name", "last_name", "email")
+    ordering = ("email",)
--- papapay/user/models.py
+++ papapay/user/models.py
@@ -1,8 +1,36 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
 
+class UserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
+    def _create_user(self, email, first_name, last_name, password, **extra_fields):
+        if not email or not first_name or not last_name:
+            raise ValueError('User must have email address, first name and last name.')
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, first_name, last_name, password=None):
+        return self._create_user(email, first_name, last_name, password)
+
+    def create_superuser(self, email, first_name, last_name, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, first_name, last_name, password, **extra_fields)
+
+
 class User(AbstractUser):
+    username = None
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
     email = models.EmailField(
@@ -11,8 +39,10 @@ class User(AbstractUser):
         unique=True,
     )
 
+    objects = UserManager()
+
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def __str__(self):
         return self.email
