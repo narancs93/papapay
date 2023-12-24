f465afd421925181c14c78b81174281c672f8e8c
Lajos Seyler <lajos.seyler@gmail.com>
2023-12-24 11:22:47 +0100

    feat - add management command to load call prefixes into db

--- papapay/common/management/commands/load_app_data.py
+++ papapay/common/management/commands/load_app_data.py
@@ -1,5 +1,8 @@
-import pycountry
 from django.core.management.base import BaseCommand
+
+from phonenumbers import country_code_for_region
+
+from pycountry import countries as py_countries
 
 from papapay.postal_address.models import Country
 
@@ -9,16 +12,28 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.load_countries()
+        self.load_international_call_prefixes()
 
         self.stdout.write(
             self.style.SUCCESS('Application data loaded successfully')
         )
 
     def load_countries(self):
-        for country in pycountry.countries:
+        for country in py_countries:
             Country.objects.update_or_create(
                 alpha3_code=country.alpha_3,
                 defaults={
                     'name': country.name
                 }
             )
+
+    def load_international_call_prefixes(self):
+        for country in py_countries:
+            country_code = country.alpha_2
+            call_prefix = country_code_for_region(country_code)
+            try:
+                country_obj = Country.objects.get(alpha3_code=country.alpha_3)
+                country_obj.international_call_prefix = f'+{call_prefix}'
+                country_obj.save()
+            except Country.DoesNotExist:
+                pass
