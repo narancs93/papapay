65556adb5c695fd6dd3a7a344e6285b47e9dff9c
Lajos Seyler <lajos.seyler@gmail.com>
2024-01-17 16:31:07 +0100

    dependencies - django-crispy-forms

--- config/settings.py
+++ config/settings.py
@@ -58,6 +58,8 @@ INSTALLED_APPS = [
 
     # Package apps
     'rest_framework',
+    'crispy_forms',
+    'crispy_tailwind',
 
     # Developed apps
     'papapay.user',
@@ -157,3 +159,7 @@ STATICFILES_DIRS = [root('papapay/static')]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+
+CRISPY_TEMPLATE_PACK = "tailwind"
