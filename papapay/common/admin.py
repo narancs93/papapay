4e6788ce51b32a46b9fc671867450329a5807767
Lajos Seyler <lajos.seyler@gmail.com>
2024-01-19 18:32:47 +0100

    admin - Add PageAccess to admin page

--- papapay/common/admin.py
+++ papapay/common/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from .models import PhoneNumber
+from .models import PageAccess, PhoneNumber
 
 
 @admin.register(PhoneNumber)
 class PhoneNumberAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'country', 'phone_number', 'owner')
     search_fields = ('name', 'phone_number')
+
+
+@admin.register(PageAccess)
+class PageAccessAdmin(admin.ModelAdmin):
+    list_display = ('id', 'app_name', 'url_name')
+    search_fields = ('app_name', 'url_name')
