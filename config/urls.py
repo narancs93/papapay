5e60ca2285bf8f8ca9f2927cd39ac828c8495cbc
Lajos Seyler <lajos.seyler@gmail.com>
2024-02-01 14:55:48 +0100

    fix - Fix permission_required_middleware to return 403 status code

--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from papapay.common.views import PermissionDeniedPage
-
 urlpatterns = [
-    path('forbidden', PermissionDeniedPage.as_view(), name='403-page'),
     path('admin/', admin.site.urls),
     path('', include('papapay.home.urls')),
     path('user/', include('papapay.user.urls')),
--- papapay/common/views.py
+++
@@ -1,5 +0,0 @@
-from django.views.generic import TemplateView
-
-
-class PermissionDeniedPage(TemplateView):
-    template_name = '403.html'
